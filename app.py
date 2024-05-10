68482a5ab73213e5a5e549b5fd83425408cb7c86
Konsticraft <k.craft@web.de>
2024-05-10 13:19:06 +0200

    save before V2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file, request, Response, url_for, send_from_directory, redirect
+from flask import Flask, render_template, send_file, request, Response, url_for, send_from_directory, redirect, current_app
 import os
 import subprocess
 import multiprocessing
@@ -7,17 +7,60 @@ import datetime
 import configparser
 import battery #only works on PI
 import sys
+import cam
+import time
+import shutil
+import threading
+from gpiozero import Button, TonalBuzzer
+from gpiozero.tones import Tone
 
 app = Flask(__name__)
 # Directory where your recorded videos are stored
 video_dir = "videos"
 thumb_dir = "thumbs"
+highlight_dir = "highlights"
+output_dir = "output"
+temp_dir = "temp"
+highlight_thumb_dir = "highlight_thumbs"
 config_path = "config.ini"
 recording_running = False
 recording_process = None
 config = {}
 recording_process = None
 parser = None
+battery_percentage = -1
+camera_thread = None
+button = Button(14)
+buzzer = TonalBuzzer(25)
+count = 0
+
+def buttonpress():
+    global button_press_start_time
+    button_press_start_time = time.time()
+
+def buttonrelease():
+    global button_press_start_time
+    global count
+    press_duration = time.time()-button_press_start_time
+    print(f'Press Duration: {press_duration}')
+    if press_duration > 0.01 :
+        if press_duration > 0.5 :
+            print(f'Long Press {count}')
+            if recording_running:
+                camera_thread.save(int(config["highlight_before"]), int(config["highlight_after"]))
+                buzzer.play(Tone("C4"))
+                time.sleep(0.2)
+                buzzer.play(Tone("D4"))
+                time.sleep(0.2)
+                buzzer.play(Tone("E4"))
+                time.sleep(0.2)
+                buzzer.play(Tone("A4"))
+                time.sleep(0.2)
+                buzzer.stop()
+        else:
+            print(f'Short Press {count}')
+        button_press_start_time = None
+        count+= 1
 
 def set_config():
     print("reading config file")
@@ -33,18 +76,24 @@ def settings_route():
     auto_rec = config["recording_autostart"]
     port = config["port"]
     seg_len = config["segment_length"]
-    return render_template('settings.html', auto_rec = auto_rec, port = port, seg_len = seg_len)
+    rec_bef = config["highlight_before"]
+    rec_aft = config["highlight_after"]
+    return render_template('settings.html', auto_rec = auto_rec, port = port, seg_len = seg_len, battery_percentage=battery_percentage, rec_bef = rec_bef, rec_aft = rec_aft)
 
 @app.route('/update_settings', methods=['POST'])
 def update_settings():
     rec_auto = "True" if request.form.get('Autostart') else "False"
     seg_len = request.form.get('seg_len')
     port = request.form.get('port')
+    rec_bef = request.form.get('rec_bef')
+    rec_aft = request.form.get('rec_aft')
     global config
     config = {
          "recording_autostart" : rec_auto,
          "segment_length" : seg_len,
-         "port" : port
+         "port" : port,
+         "highlight_before" : rec_bef,
+         "highlight_after" : rec_aft
     }
     global parser
     parser["DEFAULT"] = config
@@ -76,12 +125,34 @@ def get_videos():
               "timestr" : timestr,
               "thumb_path" : thumb
 		})
+    videos = sorted(videos, key = lambda d: d["timestamp"], reverse = True)
+    return videos
+
+def get_highlights():
+    videos = []
+    for file in os.listdir(highlight_dir):
+        filename = file
+        time = datetime.datetime.strptime(file, "highlight_%Y-%m-%d_%H:%M:%S.mp4")
+        timestr = datetime.datetime.strftime(time, "%d.%m.%Y %H:%M")
+        date = datetime.datetime.strftime(time, "%Y-%m-%d")
+        #thumb = os.path.join(thumb_dir,file.replace("mp4", "png")).replace("\\","/")
+        thumb = file.replace("mp4", "png")
+        videos.append({
+              "filename" : file,
+              "timestamp" : time,
+              "date" : date,
+              "timestr" : timestr,
+              "thumb_path" : thumb
+		})
+    videos = sorted(videos, key = lambda d: d["timestamp"], reverse = True)
     return videos
 
 @app.route('/exit')
 def shutdown_server():
     if recording_running:
-        recording_process.send_signal(signal.SIGINT)
+        #recording_process.send_signal(signal.SIGINT)
+        camera_thread.stop()
+        camera_thread.join()
     os.kill(os.getpid(), signal.SIGINT)
     return "Shutting down server ..."
 
@@ -90,19 +161,25 @@ def signal_handler(a,b):
     print("signal handler triggered")
     global recording_running
     global recording_process
+    global camera_thread
     if recording_running:
-        recording_process.send_signal(multiprocessing.signal.SIGINT)
+        # recording_process.send_signal(multiprocessing.signal.SIGINT)
+        camera_thread.stop()
+        camera_thread.join()
+    os.kill(os.getpid(), signal.SIGINT)
     return "interrupt signal"
 
 def start_recording_noroute():
+    global camera_thread
     global recording_running
-    global recording_process
     if not recording_running:
+        camera_thread = cam.Camera(int(config["segment_length"]))
+        camera_thread.start()
         recording_running = True
-        recording_process = subprocess.Popen(["python3", "cam.py"])
     else:
-        print("trying to stop recording")
-        recording_process.send_signal(signal.SIGINT)
+        print("stopping camera thread")
+        camera_thread.stop()
+        camera_thread.join()
         recording_running = False
 
 
@@ -114,11 +191,14 @@ def start_recording_route():
 @app.route('/home')
 @app.route('/')
 def index():
+    highlight_list = get_highlights()
     video_list = get_videos()
+    global battery_percentage
     battery_percentage = battery.get_battery() #only works on PI
     # battery_percentage = 100
     return render_template('home.html',
                            video_list=video_list,
+                           highlight_list = highlight_list,
                            recording_running=recording_running,
                            battery_percentage=battery_percentage)
 
@@ -131,34 +211,111 @@ def serve_video(video_name):
 
 @app.route('/thumbs/<path:thumb_name>')
 def serve_thumb(thumb_name):
-    return send_from_directory("thumbs", thumb_name)
+    if thumb_name.startswith("highlight"):
+        return send_from_directory(highlight_thumb_dir, thumb_name)
+    else:
+        return send_from_directory(thumb_dir, thumb_name)
+
+@app.route('/thumbs/<path:thumb_name>')
+def serve_highlight_thumb(thumb_name):
+    return send_from_directory(highlight_thumb_dir, thumb_name)
+
 
 @app.route('/stream/<path:video_name>')
 def stream_video(video_name):
-    def generate():
+    if video_name.startswith("highlight"):
+        video_path = os.path.join(highlight_dir, video_name)
+    else:
         video_path = os.path.join(video_dir, video_name)
+    size = os.stat(video_path)
+    size = size.st_size
+    
+    chunk_size = (10 ** 6) * 5
+    #start = int(re.sub("\D","", headers["range"]))
+    #end = min(start + chunk_size, size-1)
+    start, end = parse_range_header(request.headers.get('Range'), size)
+    content_length = end - start + 1
+    
+    def generate(video_path, start, chunk_size):
         with open(video_path, 'rb') as video_file:
-            while True:
-                data = video_file.read(1024)
-                if not data:
-                    break
-                yield data
+            video_file.seek(start)
+            chunk = video_file.read(chunk_size)
+        return chunk
+    
+    #response = Response(generate(), mimetype='video/mp4')
+    
+    headers = {
+        "Content-Range" : f'bytes {start}-{end}/{size}',
+        'Accept-Ranges' : 'bytes',
+        "Content-Length" : content_length,
+        "Content-Type":"video/mp4",
+        }
+        
+    return current_app.response_class(generate(video_path, start, chunk_size), 206, headers)
+    #file_size = os.path.getsize(video_path)
+    #response.headers.add('Content-Length', str(file_size))
+    
+    #range_header = request.headers.get('Range')
+    if False:
+        start, end = parse_range_header(range_header, file_size)
+        print(f'start: {start} end: {end}')
+        if start is not None and end is not None:
+            response.status_code = 206
+            response.headers.add('Content-Range', f'bytes {start}-{end}/{file_size}')
+            response.headers.add('Content-Length', str(end-start+1))
+            response.headers.add('Accept-Ranges', 'bytes')
+            with open(video_path, 'rb') as video_file:
+                video_file.seek(start)
+                return response
 
-    return Response(generate(), mimetype='video/mp4')
+    return response
 
+def parse_range_header(range_header, file_size):
+    ranges = range_header.split('=')[1].split('-')
+    start = int(ranges[0]) if ranges[0] else 0
+    end = int(ranges[1]) if ranges[1] else file_size -1
+    return start, end
+    
+def convert_old_files():
+    files = os.listdir(output_dir)
+    for file in files:
+        src_path = os.path.join(output_dir, file)
+        dest_path = os.path.join(temp_dir, file)
+        shutil.move(src_path, dest_path)
+    temp_files = os.listdir(temp_dir)
+    files.extend(temp_files)
+    thread = threading.Thread(target=convert_from_temp, args=(files,))
+    thread.start()
+    
+    
+def convert_from_temp(files):
+    print("convert")
+    for file in files:
+        file = file[:-5]
+        print(f"converting old file {file}")
+        command = f"ffmpeg -i /home/pi/PiBikeCam/temp/{file}.h264 -c:v copy -r 30 /home/pi/PiBikeCam/videos/{file}.mp4"
+        subprocess.run(command, shell = True)
+        os.remove(f"/home/pi/PiBikeCam/temp/{file}.h264")
+        
+    
 
 if __name__ == '__main__':
-    print(config_path)
-    for key in config:
-        print(key)
+    button.when_pressed = buttonpress
+    button.when_released = buttonrelease
     set_config()
+    convert_old_files()
     if config["recording_autostart"] == "True":
         start_recording_noroute()
     get_videos()
-    signal.signal(signal.SIGINT, signal_handler)
+    #signal.signal(signal.SIGINT, signal_handler)
     print("INIT complete, starting server________________")
+    buzzer.play(Tone("C4"))
+    time.sleep(0.4)
+    buzzer.play(Tone("A4"))
+    time.sleep(0.6)
+    buzzer.stop()
     try:
-        app.run(host='0.0.0.0', port=config["port"], debug=True)
+        app.run(host='0.0.0.0', port=config["port"], debug=True, threaded=True)
     except KeyboardInterrupt:
         pass
     finally:
--- cam.py
+++ cam.py
@@ -4,38 +4,67 @@ import datetime
 import os
 import subprocess
 import multiprocessing
+import threading
 
-
-def convert_to_mp4(input_file, final = False):
-    p = multiprocessing.Process(target = converter_process, args = (input_file,))
+def convert_to_mp4(input_file, timestamp, highlights, final = False):
+    p = multiprocessing.Process(target = converter_process, args = (input_file, highlights, timestamp))
     p.start()
     if final:
         p.join()
-
-def converter_process(input_file):
-    command = f"ffmpeg -i /home/pi/PiBikeCam/output/{input_file}.h264 -codec copy /home/pi/PiBikeCam/videos/{input_file}.mp4"
+        
+def converter_process(input_file, highlights, timestamp):
+    #command = f"ffmpeg -i /home/pi/PiBikeCam/output/{input_file}.h264 -b 4000k -c:v h264_omx /home/pi/PiBikeCam/videos/{input_file}.mp4"
+    command = f"ffmpeg -i /home/pi/PiBikeCam/output/{input_file}.h264 -c:v copy -r 30 /home/pi/PiBikeCam/videos/{input_file}.mp4"
     #print(f"converting with command {command}\n")
     converter_process = subprocess.run(command, shell = True)
-    
     os.remove(f"/home/pi/PiBikeCam/output/{input_file}.h264")
+    for time, before, after in highlights:
+        start = max(0, time - timestamp - before)
+        time = datetime.datetime.fromtimestamp(time-before).strftime("%Y-%m-%d_%H:%M:%S")
+        name = f'highlight_{time}'
+        command = f"ffmpeg -ss {start} -i /home/pi/PiBikeCam/videos/{input_file}.mp4 -t {after + before} -map 0 -c:v copy -r 30 /home/pi/PiBikeCam/highlights/{name}.mp4"
+        converter_process = subprocess.run(command, shell = True)
+        command_thumb = f'ffmpeg -ss {before} -i /home/pi/PiBikeCam/videos/{input_file}.mp4 -vframes 1 /home/pi/PiBikeCam/highlight_thumbs/{name}.png'
+        converter_process = subprocess.run(command_thumb, shell = True)
+        
+        
+        
+class Camera(threading.Thread):
     
-def record_video(SEGMENT_LEN = 60):
+    def __init__(self, SEGMENT_LEN):
+        super(Camera, self).__init__()
+        self.SEGMENT_LEN = SEGMENT_LEN
+        self.stop_event = threading.Event()
+        self.highlights = []    # [(timestamp, duration)]
+        self.min_time = time.time()
     
-    # Set up the camera
-    camera = picamera.PiCamera()
-    camera.resolution = (1920, 1080)  # Adjust the resolution as needed
-    camera.framerate = 30  # Adjust the framerate as needed
+    
+    def stop(self):
+        self.stop_event.set()
+        
+    def save(self, dur_before, dur_after):
+        print(f'saving from -{dur_before} to {dur_after}')
+        self.highlights.append((time.time(), dur_before, dur_after))
+        self.min_time = time.time() + dur_after + 5
+        
+        
+    
+    def run(self):
+        
+        # Set up the camera
+        camera = picamera.PiCamera()
+        camera.resolution = (1920, 1080)  # Adjust the resolution as needed
+        camera.framerate = 30  # Adjust the framerate as needed
 
-    # Create a file name template for the video segments
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    output_file_template = f"output/video_{timestamp}.h264"
-    file_name = f"video_{timestamp}"
+        # Create a file name template for the video segments
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        output_file_template = f"output/video_{timestamp}.h264"
+        file_name = f"video_{timestamp}"
 
 
 
-    # Record video continuously with annotation
-    try:
-        while True:
+        # Record video continuously with annotation
+        while not self.stop_event.is_set():
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
             camera.annotate_text = timestamp
             output_file_template = f"output/video_{timestamp}.h264"
@@ -47,20 +76,14 @@ def record_video(SEGMENT_LEN = 60):
             camera.start_recording(output_file_template)
             start_time = time.time()
 
-            while (time.time() - start_time) < SEGMENT_LEN:
+            while ((time.time() - start_time) < self.SEGMENT_LEN or self.min_time > time.time()) and not self.stop_event.is_set():
                 #print("recording running")
                 timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
                 camera.annotate_text = timestamp
                 camera.wait_recording(1)
 
             camera.stop_recording()
-            convert_to_mp4(file_name)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print("Stopping Recording") 
-        convert_to_mp4(file_name, final = True)
+            convert_to_mp4(file_name, start_time, self.highlights.copy(), self.stop_event.is_set())
+            self.highlights = []
         camera.close()
-        
-if __name__ == "__main__":
-    record_video()
+            
